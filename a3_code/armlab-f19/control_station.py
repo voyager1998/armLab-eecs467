66ad4b7da18c26f946e9fdf6d007daae93647c18
Kun Huang <huangkun@umich.edu>
2019-10-19 13:29:18 -0400

    test ik and fk

--- a3_code/armlab-f19/control_station.py
+++ a3_code/armlab-f19/control_station.py
@@ -187,7 +187,8 @@ class DisplayThread(QThread):
         while True:
             self.updateStatusMessage.emit(self.sm.status_message)
             self.updateJointReadout.emit(self.rexarm.joint_angles_fb)
-            self.updateEndEffectorReadout.emit(list(self.rexarm.rexarm_FK()))   
+            self.updateEndEffectorReadout.emit(list(self.rexarm.rexarm_FK()))
+            print(self.rexarm.rexarm_FK())
             time.sleep(0.1)
     
 """GUI Class"""
--- a3_code/armlab-f19/rexarm.py
+++ a3_code/armlab-f19/rexarm.py
@@ -30,6 +30,7 @@ class Rexarm():
         self.gripper_state = True
         self.estop = False
         """Find the physical angle limits of the Rexarm. Remember to keep track of this if you include more motors"""
+        # in radians
         self.angle_limits = np.array([
                             [-120.00, -100.00, -100.00, -100.00, -70.00],
                             [ 120.00,  20.00,  40.00,  70.00,  22.00]], dtype=np.float)*D2R
@@ -178,26 +179,42 @@ class Rexarm():
         link is the index of the joint, and it is 0 indexed (0 is base, 1 is shoulder ...)
         returns a matrix A(2D array)
         """
-
         A = [[1, 0, 0, 0], \
             [0, 1, 0, 0], \
             [0, 0, 1, 0], \
             [0, 0, 0, 1]]
-
+        if link == 0: #base
+            A = [[math.cos(theta), 0, -math.sin(theta), 0], \
+                [math.sin(theta), 0, math.cos(theta), 0], \
+                [0, -1, 1, self.dh_table[0]["d"]], \
+                [0, 0, 0, 1]]            
+        if link == 1:
+            A = [[0, 0, 1, 0], \
+                [-1, 0, 0, 0], \
+                [0, -1, 0, self.dh_table[1]["d"]], \
+                [0, 0, 0, 1]]    
+        if link == 2:
+            A = [[math.cos(theta), -math.sin(theta), 0, self.dh_table[2]["d"] * math.cos(theta)], \
+                [math.sin(theta), math.cos(theta), 0, self.dh_table[2]["d"] * math.sin(theta)], \
+                [0, 0, 1, 0], \
+                [0, 0, 0, 1]]
         return A
 
     def rexarm_FK(self, joint_num=4):
         """
         TODO: implement this function
-
         Calculates forward kinematics for rexarm
         takes a DH table filled with DH parameters of the arm
         and the link to return the position for
         returns a 4-tuple (x, y, z, phi) representing the pose of the
         desired link
         """
-
-        return (0, 0, 0, 90)
+        current_pose = np.array([0.0, 0.0, 0.0, 1.0])
+        for i in range(0, joint_num):
+            A = self.calc_A_FK(self.position[i], i)
+            current_pose = np.matmul(np.array(A), current_pose)
+        phi = math.pi/2 - self.position[0] - self.position[1] - self.position[2]
+        return (current_pose[0], current_pose[1], current_pose[2], phi)
 
     def rexarm_IK(self, pose):
         """
@@ -214,24 +231,42 @@ class Rexarm():
         """
 
 # calculated in Radian
+        pose.phi = pose.phi * D2R
+
         base_angle = np.arctan2(pose.y, pose.x)
+        if base_angle < self.angle_limits[0][0] or base_angle > self.angle_limits[1][0]:
+            print("Out of theta0 limit!")
+            return None
 
         z3 = pose.z - self.wrist_len * math.sin(pose.phi)
         l3 = math.sqrt(pose.x ** 2 + pose.y ** 2) - self.wrist_len * math.cos(pose.phi)
 
         longedge2 = l3**2 + (z3-self.dh_table[0]["d"])**2
         cosalpha = (self.dh_table[1]["d"]** 2 + longedge2 - self.dh_table[2]["d"]** 2) / (2 * self.dh_table[1]["d"] * math.sqrt(longedge2))
+        if cosalpha < -1 or cosalpha > 1:
+            return None
         alpha = math.acos(cosalpha)
         beta = np.arctan2(z3 - self.dh_table[0]["d"], l3)
         theta1 = math.pi / 2 - alpha - beta
+        if theta1 < self.angle_limits[0][1] or theta1 > self.angle_limits[1][1]:
+            print("Out of theta1 limit!")
+            return None
         
         cosgamma = (self.dh_table[1]["d"]** 2 + self.dh_table[2]["d"]** 2 - longedge2) / (2 * self.dh_table[1]["d"] * self.dh_table[2]["d"])
+        if cosgamma < -1 or cosgamma > 1:
+            return None
         gamma = math.acos(cosgamma)
         theta2 = math.pi - gamma
+        if theta2 < self.angle_limits[0][2] or theta1 > self.angle_limits[1][2]:
+            print("Out of theta2 limit!")
+            return None
 
         theta3 = math.pi/2 - theta1 - theta2 - pose.phi
+        if theta3 < self.angle_limits[0][3] or theta1 > self.angle_limits[1][3]:
+            print("Out of theta3 limit!")
+            return None
 
-# convert all to Degree
-        joint_angles = [base_angle * R2D, theta1 * R2D, theta2 * R2D, theta3 * R2D]
+# joint angles are in radians
+        joint_angles = [base_angle, theta1, theta2, theta3]
         
         return joint_angles
